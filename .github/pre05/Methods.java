befaa821bdac4a3af71c5dabe60498826d8d61b6
Shana Elizabeth Henry <57843914+mathiskey@users.noreply.github.com>
2022-03-08 23:28:12 +0000

    I did 4 programs for Pre02.

---
+++ .github/pre05/Methods.java
@@ -0,0 +1,14 @@
+/** Exercise #2*/
+
+public static boolean  isDivisible(int n, int m) {
+    if (mathRemainder n%m == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/** Exercise #3*/
+
+
+/** Exercise #8*/
---
+++ .github/pre06/Methods.java
@@ -0,0 +1,20 @@
+/** Exercise #2*/
+import java.io.*;
+import java.util.*;
+
+public class Methods {
+    public static boolean main (String[] args){
+      
+    public static boolean isDivisible(int n, int m) {
+    if (n % m == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+}
+
+/** Exercise #3*/
+
+
+/** Exercise #8*/
