ab584642c53acb5809ef8b85a36e292751d3ed8b
Shana Elizabeth Henry <57843914+mathiskey@users.noreply.github.com>
2022-03-14 22:07:28 +0000

    I finished pre06

--- .github/pre07/Max.java
+++ .github/pre07/Max.java
@@ -13,3 +13,15 @@ public class Max {
 }
 
 //Write a method called indexOfMax that takes an array of integers and returns the index of the largest element. Can you write this method using an enhanced for loop? Why or why not?
+
+public int getIndexOfLargest( int[] array )
+{
+  if ( array == null || array.length == 0 ) return -1; // null or empty
+
+  int largest = 0;
+  for ( int i = 1; i < array.length; i++ )
+  {
+      if ( array[i] > array[largest] ) largest = i;
+  }
+  return largest; // position of the first largest found
+}
