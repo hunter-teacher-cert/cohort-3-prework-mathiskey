9d7582fa542b47b52298c4967f81d3e1341efe12
Shana Elizabeth Henry <57843914+mathiskey@users.noreply.github.com>
2022-03-16 23:24:50 +0000

    I finished pre07.

--- .github/pre07/Array.java
+++ .github/pre07/Array.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 public class Array {
 
     public static void main (String[] args) {
-     double[] squareArry = powArray(new double[]{10,20,30},3);
+      double[] squareArray = powArray(new double[]{1,2,3},3);
+      int[] counts = histogram (new int []{1, 2, 3}, 5);
+      for (int value: counts) {
+        System.out.println(value);
+      }
+    }
 
-        }
-
-        public static double[] powArray (double a[],int z){
-            double[] b = new double[a.length];
-            for (int i = 0; i < a.length; i++) {
-                b[i] = Math.pow(a[i], z);
-                System.out.print(b[i]);
-            }
-            return b;
+    public static double[] powArray (double a[],int z){
+      double[] b = new double[a.length];
+      for (int i = 0; i < a.length; i++) {
+        b[i] = Math.pow(a[i], z);
+        System.out.println(b[i]);
+      }
+      return b;
+    }
+    public static int[] histogram (int a[], int c){
+      int[] counts = new int[c];
+      for (int b : a) {
+        counts[b]++;
+      }
+      return counts;
     }
 }
-//double[] a = new double[a*a];
--- .github/pre07/Max.java
+++ .github/pre07/Max.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 
 public class Max {
 
-    public static void indexOfMax (String[] args) {
+    public static int indexOfMax (int[] array ) {
+    
+      if ( array == null || array.length == 0 ) 
+        return -1; // null or empty
+      int largest = 0;
+      for ( int i = 1; i < array.length; i++ ){
+        if ( array[i] > array[largest] ) largest = i;
+      }
+      return largest; // position of the first largest found
+    }  
+
+    public static void main (String[] array) {
       
     }
-    public static void main (String[] args) {
-      
-    }
-}
-
-//Write a method called indexOfMax that takes an array of integers and returns the index of the largest element. Can you write this method using an enhanced for loop? Why or why not?
-
-public int getIndexOfLargest( int[] array )
-{
-  if ( array == null || array.length == 0 ) return -1; // null or empty
-
-  int largest = 0;
-  for ( int i = 1; i < array.length; i++ )
-  {
-      if ( array[i] > array[largest] ) largest = i;
-  }
-  return largest; // position of the first largest found
 }
